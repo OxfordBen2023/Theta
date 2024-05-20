0f455c4e2e18dd256e37b41b0673f8c871fb35b2
Benoit <benoit.nicodeme@gmail.com>
2024-05-20 16:46:14 +0200

    remove __pycache__. added a gitignore. And a few minor module importations removed as well.

--- src/PlayerNameDialog.py
+++ src/PlayerNameDialog.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QGridLayout, QVBoxLayout, QGroupBox, QPushButton, QLabel, QDialog, QLineEdit
+from PyQt5.QtWidgets import QGridLayout, QVBoxLayout, QGroupBox, QPushButton, QDialog, QLineEdit
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize
 from random import randint
